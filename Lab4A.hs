16fc55fd90931d136192bc3b1dd034c3a3c2e9ea
Magnus Eld <the_proffs@yahoo.se>
2019-10-18 19:41:04 +0200

    Major cleanup

--- Lab4A.hs
+++ Lab4A.hs
@@ -4,46 +4,26 @@
 import Poly
 import Test.QuickCheck
 
---
 -- Use the following simple data type for binary operators
 data BinOp = AddOp | MulOp
 
 --------------------------------------------------------------------------------
 -- * A1
--- Define a data type Expr which represents three kinds of expression:
--- binary operators (use BinOp as a helper type) applied to two expressions,
--- numbers (use Int), and exponentiation x^n.
--- Note that since we consider expressions containing just a single variable,
--- x, your data type should not use String or Char anywhere, since this is
--- not needed.
 
 data Expr = Const Int
           | Bin BinOp Expr Expr
           | Expo Int
 
-
-ex1 = Bin AddOp (Const 4) (Const 3) -- 4 + 3     = 7
-ex0 = Bin AddOp (Expo 4)  (Const 3)
-ex2 = Bin MulOp (Const 6) (Const 9) -- 6 * 9     = 54
-ex3 = Bin AddOp ex1 ex2         -- 7 + 54    = 61
-ex4 = Bin MulOp ex3 (Expo 5)        -- 61 * 5    = 305
-ex5 = Bin MulOp ex4 (Expo 4)        -- 305 * x^4 = 190625
-ex6 = Bin AddOp ex4 ex5
-
 --------------------------------------------------------------------------------
 -- * A2
--- Define the data type invariant that checks that exponents are never negative
+
 prop_Expr :: Expr -> Bool
 prop_Expr (Expo n)      = n > 0
 prop_Expr (Bin _ e1 e2) = prop_Expr e1 && prop_Expr e2
 prop_Expr _             = True
 
-
 --------------------------------------------------------------------------------
 -- * A3
--- Make Expr an instance of Show (along the lines of the example in the lecture)
--- You can use Haskell notation for powers: x^2
--- You should show x^1 as just x.
 
 instance Show Expr where
   show = showExpr
@@ -58,17 +38,9 @@ showExpr (Expo n)          = "x^" ++ show n
 
 showFactor e@(Bin AddOp _ _) = "(" ++ showExpr e ++ ")"
 showFactor e                 = showExpr e
+
 --------------------------------------------------------------------------------
 -- * A4
--- Make Expr and instance of Arbitrary.
--- Now you can check the data type invariant that you defined in A3 using
--- quickCheck
-
--- (Optional)
--- Add a definition of function shrink :: Expr -> [Expr] to Arbitrary
--- which gives hints to quickCheck on possible smaller expressions that it
--- could use to find a smaller counterexample for failing tests
-
 
 instance Arbitrary Expr
   where arbitrary = rExpr =<< choose (1,4)
@@ -85,8 +57,6 @@ rSimple = do n <- choose (0,9)
 
 --------------------------------------------------------------------------------
 -- * A5
--- Define the eval function which takes a value for x and an expression and
--- evaluates it
 
 eval :: Int -> Expr -> Int
 eval x (Expo n)          = x^n
@@ -96,11 +66,6 @@ eval x (Bin AddOp e1 e2) = eval x e1 + eval x e2
 
 --------------------------------------------------------------------------------
 -- * A6
--- Define
---exprToPoly :: Expr -> Poly
--- Which converts an expression into a polynomial.
--- Here it is important to think recursively to just solve the bigger problem
--- by solving the smaller problems and combining them in the right way.
 
 exprToPoly :: Expr -> Poly
 exprToPoly (Const n)         = fromList [n]
@@ -108,16 +73,13 @@ exprToPoly (Expo n)          = fromList (1:replicate n 0)
 exprToPoly (Bin AddOp e1 e2) = exprToPoly e1 + exprToPoly e2
 exprToPoly (Bin MulOp e1 e2) = exprToPoly e1 * exprToPoly e2
 
--- Define (and check) prop_exprToPoly, which checks that evaluating the
--- polynomial you get from exprToPoly gives the same answer as evaluating
--- the expression
 
 prop_exprToPoly :: Int -> Expr -> Bool
 prop_exprToPoly x e = eval x e == evalPoly x (exprToPoly e)
 
 --------------------------------------------------------------------------------
 -- * A7
--- Now define the function going in the other direction,
+
 polyToExpr :: Poly -> Expr
 polyToExpr poly = listToExpr 0 $ reverse $ toList poly
   where
@@ -131,22 +93,18 @@ polyToExpr poly = listToExpr 0 $ reverse $ toList poly
 
 
 
--- Write (and check) a quickCheck property for this function similar to
--- question 6.
+prop_polyToExpr :: Int -> Poly -> Bool
 prop_polyToExpr x poly = eval x (polyToExpr poly) == evalPoly x poly
 
 --------------------------------------------------------------------------------
 -- * A8
--- Write a function
+
 simplify :: Expr -> Expr
 simplify = polyToExpr . exprToPoly
--- which simplifies an expression by converting it to a polynomial
--- and back again
-
 
 --------------------------------------------------------------------------------
 -- * A9
--- Write a quickCheck property
+
 prop_noJunk :: Expr -> Bool
 prop_noJunk expr = noJunk (simplify expr)
   where
@@ -159,10 +117,5 @@ prop_noJunk expr = noJunk (simplify expr)
     noJunk (Bin _ e1 e2) = prop_noJunk e1 || prop_noJunk e2
     noJunk (Expo 0)        = False
     noJunk _               = True
---that checks that a simplified expression does not contain any "junk":
---where junk is defined to be multiplication by one or zero,
---addition of zero, addition or multiplication of numbers, or x to the
---power zero. (You may need to fix A7)
-
 
 --------------------------------------------------------------------------------
