a15a517e816a7850d08f8d323fe9ba725fa8eccb
Magnus Eld <the_proffs@yahoo.se>
2019-10-18 19:45:20 +0200

    Added comments

--- Lab4A.hs
+++ Lab4A.hs
@@ -9,14 +9,15 @@ data BinOp = AddOp | MulOp
 
 --------------------------------------------------------------------------------
 -- * A1
-
+-- An expression can consists additions and multiplications
+-- of numbers and x raised to a power
 data Expr = Const Int
           | Bin BinOp Expr Expr
           | Expo Int
 
 --------------------------------------------------------------------------------
 -- * A2
-
+-- Property of an Expr: Exponents should never be negative
 prop_Expr :: Expr -> Bool
 prop_Expr (Expo n)      = n > 0
 prop_Expr (Bin _ e1 e2) = prop_Expr e1 && prop_Expr e2
@@ -41,7 +42,7 @@ showFactor e                 = showExpr e
 
 --------------------------------------------------------------------------------
 -- * A4
-
+-- Expr is an instance of Arbitrary
 instance Arbitrary Expr
   where arbitrary = rExpr =<< choose (1,4)
 
@@ -57,7 +58,7 @@ rSimple = do n <- choose (0,9)
 
 --------------------------------------------------------------------------------
 -- * A5
-
+-- Evaluates an expression with a given value of x
 eval :: Int -> Expr -> Int
 eval x (Expo n)          = x^n
 eval _ (Const n)         = n
@@ -66,7 +67,7 @@ eval x (Bin AddOp e1 e2) = eval x e1 + eval x e2
 
 --------------------------------------------------------------------------------
 -- * A6
-
+-- Converts a expression to an polynomial
 exprToPoly :: Expr -> Poly
 exprToPoly (Const n)         = fromList [n]
 exprToPoly (Expo n)          = fromList (1:replicate n 0)
@@ -79,7 +80,7 @@ prop_exprToPoly x e = eval x e == evalPoly x (exprToPoly e)
 
 --------------------------------------------------------------------------------
 -- * A7
-
+-- Converts a polynomial to an expression
 polyToExpr :: Poly -> Expr
 polyToExpr poly = listToExpr 0 $ reverse $ toList poly
   where
@@ -92,19 +93,22 @@ polyToExpr poly = listToExpr 0 $ reverse $ toList poly
     listToExpr n (x:xs) = Bin AddOp (Bin MulOp (Const x) (Expo n)) (listToExpr (n+1) xs)
 
 
-
+-- Property of a polyToExpr: evaluating a plynomial should be equal to
+-- converting the polynomial to an expression and then evaluating it
 prop_polyToExpr :: Int -> Poly -> Bool
 prop_polyToExpr x poly = eval x (polyToExpr poly) == evalPoly x poly
 
 --------------------------------------------------------------------------------
 -- * A8
-
+-- Simplifies an expression by collecting terms
 simplify :: Expr -> Expr
 simplify = polyToExpr . exprToPoly
 
 --------------------------------------------------------------------------------
 -- * A9
-
+-- Property of a simplified expression:
+-- Should not contain multiplication by one or zero, addition of zero,
+-- addition or multiplication of constants, or x to the power of zero
 prop_noJunk :: Expr -> Bool
 prop_noJunk expr = noJunk (simplify expr)
   where
